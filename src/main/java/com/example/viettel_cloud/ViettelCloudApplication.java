5df3139b9097217ffe4a9358e428f96c01251b11
Trung Vũ <130747543+Trung381@users.noreply.github.com>
2025-07-10 15:24:08 +0700

    create source

---
+++ src/main/java/com/example/viettel_cloud/ViettelCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.viettel_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViettelCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViettelCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/viettel_cloud/ViettelCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viettel_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViettelCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

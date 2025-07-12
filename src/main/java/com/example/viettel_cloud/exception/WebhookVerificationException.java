1bb8f1b833e875d2e7005a567a660fd1332742df
trung381 <130747543+Trung381@users.noreply.github.com>
2025-07-12 08:33:01 +0700

    update

--- src/main/java/com/example/viettel_cloud/exception/WebhookVerificationException.java
+++ src/main/java/com/example/viettel_cloud/exception/WebhookVerificationException.java
@@ -3,7 +3,6 @@ package com.example.viettel_cloud.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// Exception này sẽ tự động trả về HTTP 400 Bad Request khi được ném ra từ Controller
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class WebhookVerificationException extends RuntimeException {
     public WebhookVerificationException(String message) {
